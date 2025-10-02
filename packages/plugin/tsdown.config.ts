8f36e46da079e07319a182436b2420aa911e2724
Columsys <wszym606@163.com>
2025-10-03 00:38:14 +0800

    chore(plugin): update tsdown build configuration for @zessjs/vite-plugin module

--- packages/plugin/tsdown.config.ts
+++ packages/plugin/tsdown.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/plugin.ts'],
   format: ['cjs', 'esm'],
-  target: 'node18.0',
   clean: true,
   dts: true,
 })
