cd33b63428d92b01c34828ef1e1c41f44d752df0
Columsys <wszym606@163.com>
2025-10-04 22:26:58 +0800

    perf(plugin): optimize esbuild transformation and plugin execution order

--- packages/plugin/src/plugin.ts
+++ packages/plugin/src/plugin.ts
@@ -1,6 +1,11 @@
 import process from 'node:process'
 import { compile } from '@zessjs/compiler'
-import { createFilter, type FilterPattern, type Plugin } from 'vite'
+import {
+  createFilter,
+  transformWithEsbuild,
+  type FilterPattern,
+  type Plugin,
+} from 'vite'
 
 export default function zess(
   options: {
@@ -15,28 +20,36 @@ export default function zess(
   )
   return {
     name: 'zess',
+    enforce: 'pre',
     config: () => ({
-      esbuild: {
-        jsx: 'preserve',
-      },
       resolve: {
         conditions: ['zess'],
       },
       optimizeDeps: {
-        esbuildOptions: {
-          jsx: 'preserve',
-        },
         force: true,
         include: ['@zessjs/core'],
         exclude: ['@zessjs/router'],
       },
     }),
-    transform(code, id) {
+    async transform(code, id) {
       if (!filter(id)) return
+      let sourcemap = this.getCombinedSourcemap()
+      if (id.endsWith('.tsx')) {
+        const transformed = await transformWithEsbuild(
+          code,
+          id,
+          {
+            jsx: 'preserve',
+          },
+          sourcemap,
+        )
+        code = transformed.code
+        sourcemap = transformed.map
+      }
       return compile(code, {
         file: id,
+        sourcemap,
         sourceRoot: process.cwd(),
-        sourcemap: this.getCombinedSourcemap(),
         modulePath: options.modulePath ?? '@zessjs/core',
       })
     },
